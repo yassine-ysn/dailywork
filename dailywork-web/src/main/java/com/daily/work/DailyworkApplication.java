3ee61df7141cf7fbbc6e22ac4e80aafbd1e28267
yassine-ysn <ysn.talbi@gmail.com>
2020-04-04 20:30:56 +0200

    Ajout de template PET_CLINIC

--- dailywork-web/src/main/java/com/daily/work/DailyworkApplication.java
+++ dailywork-web/src/main/java/com/daily/work/DailyworkApplication.java
@@ -1,13 +1,15 @@
 package com.daily.work;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @SpringBootApplication
-public class DailyworkApplication {
+public class DailyworkApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailyworkApplication.class, args);
 	}
-
 }
